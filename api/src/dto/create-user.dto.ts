beb97de077f1ae4966ab47b4db50cf6faad316f4
backupvs <dmitryholubev@gmail.com>
2023-03-24 18:27:33 +0200

    Changed username validation rule

--- api/src/dto/create-user.dto.ts
+++ api/src/dto/create-user.dto.ts
@@ -1,8 +1,17 @@
-import { IsNotEmpty, IsString, IsStrongPassword, MinLength } from "class-validator";
+import { IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
 
 export class CreateUserDto {
     @IsString()
     @IsNotEmpty()
+    @Matches(
+        /^[a-zA-Z][a-zA-Z0-9_]{3,20}[a-zA-Z0-9]$/g,
+        {
+            message:
+                `Username must start with a letter (uppercase or lowercase), ` +
+                `be between 4 and 20 characters long, and only contain letters, numbers, and underscores. ` +
+                `Must end with a letter or number.`
+        }
+    )
     readonly username: string;
 
     // @IsStrongPassword()
